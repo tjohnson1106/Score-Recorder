8127c4a913f90b9e11e486d14695be1ea6948db3
Thomas Johnson <tjohnson1106@gmail.com>
2018-12-12 13:52:00 -0500

    created initial routing configuration

---
+++ src/components/NewScore.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const NewScore = () => <div>New Score</div>;
+
+export default NewScore;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { InMemoryCache, ApolloClient, HttpLink } from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "tachyons";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import App from "./App";
+import NewScore from "./components/NewScore";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://api.graph.cool/simple/v1/cjbl0bxmq04570186hqlvgpmg"
+  })
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Router>
+      <Route exact path="/" component={App} />
+      <Route exact path="/new-score" component={NewScore} />
+    </Router>
+  </ApolloProvider>,
+
+  <App />,
+
+  document.getElementById("root")
+);
+
 serviceWorker.unregister();
